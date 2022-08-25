3845cf6c67d63a282e38709c8029c26fdd932863
Johdasgran <dampig14@gmail.com>
2022-08-24 20:53:01 -0500

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,14 @@ function App() {
      <Card type="espresso" name="espresso"></Card>
      <Card type="hanzelnut_latte" name="hanzelnut_latte"></Card>
      <Card type="coffee" name="coffee" ></Card>
+
+     
      <Card type="espresso" name="espresso"></Card>
      <Card type="hanzelnut_latte" name="hanzelnut_latte"></Card>
      <Card type="coffee" name="coffee"></Card>
      <Card type="espresso" name="espresso"></Card>
+
+
 
 
 
